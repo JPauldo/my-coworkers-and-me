7efc89c60717f9b082c7fec94d6f540a9cfda51b
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-06 18:18:35 -0500

    Added structure to classes and declared functions for main scripts

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const employeePage = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+function getEQs(questionKey) {
+}
+
+function promptQuestions(questions) {
+}
+
+function writeFile(fileName, employeeData) {
+}
+
+function init() {
+  // 
+}
+
+init();
