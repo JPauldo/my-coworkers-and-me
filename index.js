9f53cb3ae3b5795de7fee0fadb5f90f6cda02942
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-07 17:04:04 -0500

    Added function for creating Employee objects and fixing the Intern & Engineer classes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+function createEmployee(data) {
+}
+
 function getEQs(questionKey) {
 }
 
